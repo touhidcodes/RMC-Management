d1346d1385edc3f1bc6f9e61d7ccd85cda037ab2
touhidcodes <touhidcodes@gmail.com>
2024-07-29 22:25:20 +0600

    feat: react hook form and MUI added

---
+++ src/app/(withFormsLayout)/forms/kyc/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h2>Welcome to the kyc page</h2>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(withFormsLayout)/layout.tsx
@@ -0,0 +1,9 @@
+const FormLayout = () => {
+  return (
+    <div className="min-h-screen ">
+      <div>Resume layout</div>
+    </div>
+  );
+};
+
+export default FormLayout;
