d44743239901c8127e2464437f3ea36af181eb75
touhidcodes <touhidcodes@gmail.com>
2024-08-07 23:27:41 +0600

    x

--- src/components/KYC Form/kycForm.tsx
+++ src/components/KYC Form/kycForm.tsx
@@ -1,43 +1,154 @@
-import { Box, Button, Grid } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { FieldValues } from "react-hook-form";
 import TCForm from "../Forms/TCForm";
 import TCInput from "../Forms/TCInput";
-import { useDispatch } from "react-redux";
-import { updateKycData } from "@/redux/features/kycSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useEffect } from "react";
+import { setKycData } from "@/redux/features/kycSlice";
 
-// KYCForm component
 const KYCForm = () => {
   const dispatch = useDispatch();
+  const kycData = useSelector((state: RootState) => state.kyc);
 
   const handleSubmit = (data: FieldValues) => {
-    console.log("Form Data:", data);
-    // You can process the form data here, e.g., send it to a server
-    dispatch(updateKycData(data));
+    dispatch(setKycData(data));
   };
 
+  useEffect(() => {
+    // Handle any side effects if necessary when kycData changes
+  }, [kycData]);
+
   return (
-    <Box sx={{ maxWidth: 500, mx: "auto", p: 2 }}>
+    <Box
+      sx={{
+        maxWidth: 600,
+        mx: "auto",
+        p: 4,
+        boxShadow: 3,
+        borderRadius: 2,
+        bgcolor: "background.paper",
+        mt: 4,
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{ mb: 2, textAlign: "center", fontWeight: "bold" }}
+      >
+        KYC Profile Form
+      </Typography>
       <TCForm onSubmit={handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <TCInput name="name" label="Name" required fullWidth />
+            <TCInput name="name" label="Name of Customer" required fullWidth />
           </Grid>
           <Grid item xs={12}>
             <TCInput
-              name="email"
-              label="Email"
-              type="email"
+              name="fathersName"
+              label="Father's Name"
               required
               fullWidth
             />
           </Grid>
           <Grid item xs={12}>
-            <TCInput name="phone" label="Phone" type="tel" required fullWidth />
+            <TCInput
+              name="mothersName"
+              label="Mother's Name"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput name="spouseName" label="Spouse's Name" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput
+              name="nationality"
+              label="Nationality"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TCInput name="passportNo" label="Passport No" required fullWidth />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TCInput
+              name="passportIssueDate"
+              label="Date of Issue"
+              type="date"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput
+              name="passportPlaceOfIssue"
+              label="Place of Issue"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput
+              name="nid"
+              label="National ID No (For Bangladeshi National)"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput
+              name="dob"
+              label="Date of Birth"
+              type="date"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput name="occupation" label="Occupation" fullWidth />
           </Grid>
           <Grid item xs={12}>
             <TCInput name="address" label="Address" required fullWidth />
           </Grid>
-          {/* Add more fields as necessary */}
+          <Grid item xs={12}>
+            <TCInput
+              name="phone"
+              label="Telephone No / Mobile No"
+              type="tel"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput name="countryOfVisit" label="Country of Visit" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput
+              name="purposeOfTravel"
+              label="Purpose of Travel"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TCInput
+              name="dateOfArrival"
+              label="Date of Arrival (Approx)"
+              type="date"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TCInput
+              name="dateOfDeparture"
+              label="Date of Departure (Approx)"
+              type="date"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TCInput name="sourceOfFund" label="Source of Fund" fullWidth />
+          </Grid>
+          {/* Additional Fields can be added as necessary */}
           <Grid item xs={12}>
             <Button type="submit" variant="contained" color="primary" fullWidth>
               Submit
@@ -45,6 +156,31 @@ const KYCForm = () => {
           </Grid>
         </Grid>
       </TCForm>
+
+      {/* Preview Section */}
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h6" sx={{ mb: 2, fontWeight: "bold" }}>
+          Preview
+        </Typography>
+        <Grid container spacing={2}>
+          {kycData.dateOfArrival && (
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                13. Date of Arrival (Approx):{" "}
+                <span className="font-medium">{kycData.dateOfArrival}</span>
+              </Typography>
+            </Grid>
+          )}
+          {kycData.dateOfDeparture && (
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                13. Date of Departure (Approx):{" "}
+                <span className="font-medium">{kycData.dateOfDeparture}</span>
+              </Typography>
+            </Grid>
+          )}
+        </Grid>
+      </Box>
     </Box>
   );
 };
--- src/components/KYC Preview/KYCPreview.tsx
+++ src/components/KYC Preview/KYCPreview.tsx
@@ -57,6 +57,8 @@ const KYCPreview = () => {
               <br />
               (A) Passport No:{" "}
               <span className="font-medium">{kycData.passportNo}</span> <br />
+              (A) Passport No:{" "}
+              <span className="font-medium">{kycData.passportNo}</span> <br />
               (B) Date of Issue:{" "}
               <span className="font-medium">
                 {kycData.passportIssueDate}
