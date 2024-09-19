73a58785c87af27b7dfed99fd23e0128ef95b18c
touhidcodes <touhidcodes@gmail.com>
2024-09-20 03:45:50 +0600

    feat: form options added

---
+++ src/constants/formOptions.ts
@@ -0,0 +1,75 @@
+const transactionOptions = [
+  { value: "Buy", label: "Buy" },
+  { value: "Sell", label: "Sell" },
+];
+
+const genderOptions = [
+  { value: "Male", label: "Male" },
+  { value: "Female", label: "Female" },
+];
+
+const travelOptions = [
+  { value: "Arrival", label: "Arrival" },
+  { value: "Departure", label: "Departure" },
+];
+
+const travelPurposeOptions = [
+  { value: "Transit", label: "Transit" },
+  { value: "Tourist", label: "Tourist" },
+  { value: "Medical", label: "Medical" },
+  { value: "Medical Attendant", label: "Medical Attendant" },
+  { value: "Business", label: "Business" },
+  { value: "Employment", label: "Employment" },
+  { value: "Project", label: "Project" },
+  { value: "Intern", label: "Intern" },
+  { value: "Film", label: "Film" },
+  { value: "Student", label: "Student" },
+  { value: "Research", label: "Research" },
+  { value: "Conference", label: "Conference" },
+  { value: "Mountaineering", label: "Mountaineering" },
+  { value: "Missionary", label: "Missionary" },
+  { value: "Journalist", label: "Journalist" },
+  { value: "Entry", label: "Entry" },
+  { value: "Diplomat", label: "Diplomat" },
+  {
+    value: "Official / Miscellaneous",
+    label: "Official / Miscellaneous",
+  },
+  {
+    value: "Visitor",
+    label: "Visitor",
+  },
+  {
+    value: "Double Entry",
+    label: "Double Entry",
+  },
+];
+
+const fundSourceOptions = [
+  { value: "Personal Savings", label: "Personal Savings" },
+  { value: "Investment Income", label: "Investment Income" },
+  { value: "Loans", label: "Loans" },
+  { value: "Grants", label: "Grants" },
+  { value: "Crowdfunding", label: "Crowdfunding" },
+  { value: "Sponsorship", label: "Sponsorship" },
+  { value: "Venture Capital", label: "Venture Capital" },
+  { value: "Angel Investors", label: "Angel Investors" },
+  { value: "Business Revenue", label: "Business Revenue" },
+  { value: "Family and Friends", label: "Family and Friends" },
+  { value: "Government Subsidies", label: "Government Subsidies" },
+  { value: "Partnerships", label: "Partnerships" },
+  { value: "Student", label: "Student" },
+  { value: "Teaching", label: "Teaching" },
+  { value: "Freelancing", label: "Freelancing" },
+  { value: "Consulting", label: "Consulting" },
+  { value: "Part-Time Job", label: "Part-Time Job" },
+  { value: "Internship", label: "Internship" },
+  { value: "Side Business", label: "Side Business" },
+  { value: "E-commerce", label: "E-commerce" },
+  { value: "Real Estate", label: "Real Estate" },
+  { value: "Affiliate Marketing", label: "Affiliate Marketing" },
+  { value: "Royalties", label: "Royalties" },
+  { value: "Passive Income", label: "Passive Income" },
+  { value: "Rental Income", label: "Rental Income" },
+  { value: "Investments", label: "Investments" },
+];
